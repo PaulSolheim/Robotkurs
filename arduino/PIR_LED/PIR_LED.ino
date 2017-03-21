0f90dabbedba2f6e5e81011e5210658ec6d1ff0d
Pål Grønstad Solheim <paul.solheim@gmail.com>
2017-03-21 22:36:48 +0100

    ESP Bil og PIR sensor
    Lagt til Arduino prosjekter for ESP8266 bil og PIR sensor med Lys.

---
+++ arduino/PIR_LED/PIR_LED.ino
@@ -0,0 +1,29 @@
+int ledPin = 7;
+int inputPin = 6;
+int pirState = LOW;
+int val = 0;
+ 
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(inputPin, INPUT);
+ 
+  Serial.begin(9600);
+}
+ 
+void loop(){
+  val = digitalRead(inputPin);
+  if (val == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    if (pirState == LOW) {
+      Serial.println("Motion detected!");
+      pirState = HIGH;
+    }
+  } else {
+    digitalWrite(ledPin, LOW);
+    if (pirState == HIGH){
+      Serial.println("Motion ended!");
+      pirState = LOW;
+    }
+  }
+}
+
