e7d903b6c2a790c4c42ac50770c5c464479ac80d
Pål Grønstad Solheim <paul.solheim@gmail.com>
2017-12-06 15:24:54 +0100

    farger og norsk
    Lagt inn farger og norske navn.

--- kk_python/spill.py
+++ kk_python/spill.py
@@ -1,33 +1,40 @@
 import tkinter
 from random import randint
 
-def countdown():
-    global time_left
-    
-    if time_left > 0:
-        time_left = time_left - 1
-        time_label.config(text="tid igjen: " + str(time_left))
-        
-root = tkinter.Tk()
-root.title("fargespill")
-root.geometry("475x360")
+farger_engelsk = ['Red', 'Blue', 'Green', 'Pink', 'Black',
+               'Yellow', 'Orange', 'Purple', 'Brown']
+farger_norsk = ['Rød', 'Blå', 'Grønn', 'Rosa', 'Svart',
+           'Gul', 'Oransje', 'Lilla', 'Brun']
 
-instructions = tkinter.Label(root,
+tid_igjen = 30
+
+def nedtelling():
+    global tid_igjen
+    
+    if tid_igjen > 0:
+        tid_igjen = tid_igjen - 1
+        tid_label.config(text="tid igjen: " + str(tid_igjen))
+        
+vinduet = tkinter.Tk()
+vinduet.title("fargespill")
+vinduet.geometry("475x360")
+
+instruks = tkinter.Label(vinduet,
                              text="skriv inn fargen PÅ ordet, ikke selve ordet!",
                              font=('Helvetica', 15))
 
-instructions.pack()
+instruks.pack()
 
-points_label = tkinter.Label(root,
+poeng_label = tkinter.Label(vinduet,
                              text="trykk enter for å starte.",
                              font=('Helvetica', 30))
 
-points_label.pack()
+poeng_label.pack()
 
-label = tkinter.Label(root, font=('Helvetica', 100))
+label = tkinter.Label(vinduet, font=('Helvetica', 100))
 label.pack()
 
-box = tkinter.Entry(root)
+boks = tkinter.Entry(vinduet)
 
-root.mainloop()
+vinduet.mainloop()
 
